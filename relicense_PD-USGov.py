32cdd54e7e8c71ebda52782c4900e6fc531eca69
elisfkc <elisfkc@gmail.com>
2017-03-24 14:39:27 -0400

    DVIDS Hub and DVIDS Hub Flickr

--- relicense_PD-USGov.py
+++ relicense_PD-USGov.py
@@ -163,6 +163,8 @@ searchstrings = [
 	['insource:/35591378@N03/', 'PD-POTUS', 'Files from the Obama White House Flickr stream'],
 	['insource:/nps.gov/', 'PD-USGov-NPS'],
  	['insource:/42600860@N02/', 'PD-USGov-NPS', 'Files from the U.S. National Parks Service Flickr stream'],
+	['insource:/28650594@N03', 'PD-USGov-DOD', 'Photographs by Defense Video and Imagery Distribution System'],
+	['insource:/https://www.dvidshub.net', 'PD-USGov-DOD'],
 	]
 
 searchcore = 'incategory:"PD US Government" insource:/"{{PD-USGov}}"/ '
